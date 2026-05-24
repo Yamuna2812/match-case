01f5ed4f83f002928a1de7ccc4353cd789742240
Yamuna <hyamuna2807@gmail.com>
2026-05-24 21:18:50 +0530

    Implement match statement for number input

---
+++ match31.py
@@ -0,0 +1,20 @@
+num = int(input("Enter number (1-5): "))
+
+match num:
+    case 1:
+        print("One")
+
+    case 2:
+        print("Two")
+
+    case 3:
+        print("Three")
+
+    case 4:
+        print("Four")
+
+    case 5:
+        print("Five")
+
+    case _:
+        print("Invalid number")
